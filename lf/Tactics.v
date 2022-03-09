6a6881c55e69c98e15ff29ac22fa93f4eaf48b66
superestos <superestos@gmail.com>
2022-03-09 17:17:05 +0800

    nth_error_after_last try

--- lf/Tactics.v
+++ lf/Tactics.v
@@ -742,7 +742,14 @@ Theorem nth_error_after_last: forall (n : nat) (X : Type) (l : list X),
   length l = n ->
   nth_error l n = None.
 Proof.
-  (* FILL IN HERE *) Admitted.
+  intros n X l eq. 
+  generalize dependent n.
+  induction l as [| x l' IHl'].
+  - reflexivity.
+  - simpl. intros n eq. destruct n as [| n'] eqn:E.
+    + discriminate eq.
+    + Admitted.
+
 (** [] *)
 
 (* ################################################################# *)
