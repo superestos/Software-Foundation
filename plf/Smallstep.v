b9fb34fe61d2838484a5dfa3afba09709e00ef57
superestos <superestos@gmail.com>
2022-09-24 16:33:56 +0800

    smallstep bool

--- plf/Smallstep.v
+++ plf/Smallstep.v
@@ -764,13 +764,31 @@ Definition manual_grade_for_smallstep_bools : option (nat*string) := None.
 Theorem strong_progress_bool : forall t,
   value t \/ (exists t', t --> t').
 Proof.
-  (* FILL IN HERE *) Admitted.
+  induction t.
+  - (* value tru *)
+    left. apply v_tru.
+  - (* value fls *)
+    left. apply v_fls.
+  - (* test *)
+    right. destruct t1.
+    + exists t2. apply ST_IfTrue.
+    + exists t3. apply ST_IfFalse.
+    + destruct IHt1. inversion H. inversion H.
+      exists (test x t2 t3). apply ST_If. assumption.
+Qed.
 (** [] *)
 
 (** **** Exercise: 2 stars, standard, optional (step_deterministic) *)
 Theorem step_deterministic : deterministic step.
 Proof.
-  (* FILL IN HERE *) Admitted.
+  unfold deterministic. intros x y1 y2 Hy1 Hy2.
+  generalize dependent y2.
+  induction Hy1; intros y2 Hy2;
+    inversion Hy2; subst; try solve_by_invert.
+  - reflexivity.
+  - reflexivity.
+  - apply IHHy1 in H3. rewrite H3. reflexivity.
+Qed.
 (** [] *)
 
 Module Temp5.
